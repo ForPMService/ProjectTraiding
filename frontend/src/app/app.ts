cd4a81af3fd031147e6857dc8a4b576cc182ba7a
Aleksandr <buhfan@gmail.com>
2026-06-21 13:23:36 +0300

    Frontend Project Angular 22 side

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
+import { Sidebar } from './sidebar/sidebar';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Sidebar],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
